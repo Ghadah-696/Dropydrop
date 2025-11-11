7e490aba7c6f508784e35448e21ecbfdf1de6146
Eng.Ghada <145274885+Ghadah-696@users.noreply.github.com>
2025-11-11 14:25:48 +0300

    Update functions.js

--- javascript/functions.js
+++ javascript/functions.js
@@ -52,7 +52,7 @@
 //     //     observer.observe(document.getElementById("header-container"), { childList: true, subtree: true });
 //     // });
 
-    fetch("includes/footer.html")
+    fetch("include/footer.html")
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok ' + response.statusText);
